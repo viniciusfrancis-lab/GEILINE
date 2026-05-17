8477c95d7aedc8b9798e796a901597e397f3f57b
viniciusfrancis-lab <viniciusfrancis@gmail.com>
2026-05-17 09:18:30 -0300

    Update streamlit_app_local.py

--- streamlit_app_local.py
+++ streamlit_app_local.py
@@ -18,7 +18,7 @@ st.set_page_config(page_title="Olá, sou GeiLine sua Assistente Virtual", page_i
 # ========================
 GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
 
-MODEL = "mixtral-8x7b-32768"
+MODEL = "llama-3.1-70b-versatile"
 
 ARQUIVO_DADOS = "dados/SÍNTESE GERAL DA REDE ESTADUAL DE ENSINO.xlsx"
 
